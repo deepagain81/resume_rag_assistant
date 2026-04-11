2bea3f8c731595cf9ee711c4d818b94dc846a737
Deepak Chapagain <dchapagain.dev@gmail.com>
2026-04-11 17:12:57 -0500

    [RRA-103] -Setup fast api

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,8 @@
-def main() -> None:
-    print("Hello, Python project on Mac!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def health_check() -> dict[str, str]:
+    return {"status": "ok"}
